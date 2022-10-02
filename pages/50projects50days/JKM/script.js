110b0a3aca0fb275a77deb0a0c5084846026b146
Cyberfish <1028755879@qq.com>
2022-10-02 17:04:59 +0800

    add JKM

---
+++ pages/50projects50days/JKM/script.js
@@ -0,0 +1,25 @@
+// 获取时间
+function format(num) {
+    if (num < 10) return '0' + num
+    return '' + num
+}
+let date = new Date(),
+    mon = format(date.getMonth() + 1),
+    day = format(date.getDate()),
+    hour = format(date.getHours()),
+    min = format(date.getMinutes()),
+    sec = format(date.getSeconds())
+function updateTime() {
+    hour = format(date.getHours()),
+    min = format(date.getMinutes()),
+    sec = format(date.getSeconds())
+    timeDom.innerText = hour + ':' + min + ':' + sec
+}
+
+const dateDom = document.querySelector('.date'),
+    timeDom = document.querySelector('.time')
+dateDom.innerText = mon + '月' + day + '日'
+setInterval(updateTime(), 1000)
+
+
+let edit = document.querySelector('.icon-gengduo')
